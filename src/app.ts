645dca3f9ad08b1850ad940b548c5b9c8dce9f03
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2021-09-28 21:39:26 -0700

    dom element selection und oop rendering

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+class DragenDrop {
+  // Variables
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  //Constructor
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    ); // import with all levels of deepness
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectDnD = new DragenDrop();
