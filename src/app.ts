e716106cc89449c94d33ca5bb2e214d19667027d
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2021-09-28 21:54:25 -0700

    Interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 class DragenDrop {
-  // Variables
+  // DOM element variables
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+
+  // Form field variables
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   //Constructor
   constructor() {
@@ -15,9 +20,32 @@ class DragenDrop {
       true
     ); // import with all levels of deepness
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input"; // this helps with CSS and other functionality that depends on element IDs in the DOM
+
+    //initialise form-based variables
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    // Attach the main element to the DOM
+    this.configure();
     this.attach();
   }
 
+  // Methods
+  private submitHandler(event: Event) {
+    event.preventDefault(); // prevent default submission
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
