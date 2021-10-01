3660b2ffc122388799a8923d8aaf38581e9fec8e
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2021-09-30 20:52:57 -0700

    using a getter to print a dynamic message

--- src/app.ts
+++ src/app.ts
@@ -163,6 +163,10 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+  get persons() {
+    if (this.project.people === 1) return "1 Person";
+    else return `${this.project.people} Persons`;
+  }
 
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
@@ -175,8 +179,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   configure() {}
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
